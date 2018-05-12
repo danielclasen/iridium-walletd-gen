219c57783dc5a2ed9fa402600bc037299155e47c
Daniel Clasen <mail@danielclasen.de>
2018-05-12 19:50:46 +0200

    docker-entrypoint with wallet check and .adr file generation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # iridium-walletd-gen
+
+This docker image can be used to generate an IRD Wallet in a one-shot command. 
+Additionally a .adr file will be generated containing the wallets address.
+
+## Usage
+see https://wiki.ird.cash/iridium_walletd
---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+wallet_regex=".*-w\ (\S+).*|.*--container-file=(\S+).*"
+
+if [[ "$@" =~ $wallet_regex ]]; then
+    WALLET=${BASH_REMATCH[1]}
+
+    if [ ! -f "$WALLET" ]; then
+        echo "Wallet $WALLET does not exist. Going to create a new one."
+        iridium/iridium_walletd "$@" | grep -Po ".*Address: (\S+)$" | sed -E "s/.*Address: (\S+)$/\\1/" > "$WALLET.adr"
+    else
+        echo "Wallet $WALLET already present. Nothing to do."
+        exit 0
+    fi
+
+
+
+else
+    echo "Please specify a wallet file!"
+    exit 1;
+fi
+
