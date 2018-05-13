de8fd6abebd282e47e0736aa3010f20d23ce4aea
Daniel Clasen <mail@danielclasen.de>
2018-05-13 15:14:13 +0200

    loosen wallet file permissions for easier sharing in docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,8 @@ if [[ "$@" =~ $wallet_regex ]]; then
     if [ ! -f "$WALLET" ]; then
         echo "Wallet $WALLET does not exist. Going to create a new one."
         iridium/iridium_walletd "$@" | grep -Po ".*Address: (\S+)$" | sed -E "s/.*Address: (\S+)$/\\1/" > "$WALLET.adr"
+        chmod -R 777 "$WALLET"
+        chmod -R 777 "$WALLET.adr"
     else
         echo "Wallet $WALLET already present. Nothing to do."
         exit 0
